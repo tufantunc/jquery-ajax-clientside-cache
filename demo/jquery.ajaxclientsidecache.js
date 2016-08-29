f06997f1cb3ece6168c7be718d0d8d4264723224
tufantunc <tufan.tunc.91@gmail.com>
2016-08-30 01:28:26 +0300

    çalışır hale getirildi

--- demo/jquery.ajaxclientsidecache.js
+++ demo/jquery.ajaxclientsidecache.js
@@ -46,33 +46,26 @@ var clientSideAjaxCache = {};
 
 $(document).ajaxSend(function (event, jqxhr, settings) {
 	if (typeof settings.clientSideCache !== "undefined" && settings.clientSideCache === true) {
-		var requestData = settings.data;
-		var requestUrl = settings.url;
-		var cacheData = clientSideAjaxCache.getCache(requestUrl, requestData);
-
+		var cacheData = clientSideAjaxCache.getCache(settings.url, settings.data);
 		jqxhr["settings"] = settings;
-		if (cacheData !== null) {
+		if(!!cacheData) {
+			jqxhr["responseJSON"] = cacheData;
+			jqxhr["responseText"] = JSON.stringify(cacheData);
 			jqxhr.abort();
-			//jqxhr["responseText"] = JSON.stringify(cacheData);
-			//jqxhr["responseJSON"] = cacheData;
-			//jqxhr["readyState"] = 4;
-			//jqxhr["status"] = 200;
-			//$.event.trigger("ajaxSuccess");
 		}
 	}
 });
 
-$(document).ajaxSuccess(function (event, status, cacheInfo, cacheData) {
-	if (typeof cacheInfo.clientSideCache !== "undefined") {
-		console.log(cacheInfo);
-		if (typeof cacheInfo.clientSideCache !== "undefined" && cacheInfo.clientSideCache === true) {
-			var requestData = cacheInfo.data;
-			var requestUrl = cacheInfo.url;
+$(document).ajaxSuccess(function (event, jqxhr) {
+	if(typeof jqxhr.settings.clientSideCache !== "undefined" && jqxhr.settings.clientSideCache === true) {
+		var requestData = jqxhr.settings.data;
+		var requestUrl = jqxhr.settings.url;
+		var cacheData = jqxhr.responseJSON;
 			var cacheInfo = clientSideAjaxCache.getCache(requestUrl, requestData);
 			if (cacheInfo === null) {
+				var cacheData = jqxhr.responseJSON;
 				clientSideAjaxCache.setCache(requestUrl, requestData, cacheData);
 			}
 		}
-	}
 
 });
